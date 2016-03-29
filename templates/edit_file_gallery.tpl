armtuk/tiki

commit 628a46323bea4ea205c7fa79d4271afa5d211435

[FIX] filegals: Move the default_view setting from the middle of the "items to display" section up to the top when editing file galleries where it should be
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57569 b456876b-0849-0410-b77d-98878d47e9d5

templates/edit_file_gallery.tpl
@@ -226,6 +226,29 @@ if ($(this).val() != '') {
 			{tab name="{tr}Display Settings{/tr}"}
 				<h2>{tr}Display Settings{/tr}</h2><br>
 				<div class="form-group">
+					<label class="col-sm-4 control-label" for="fgal_default_view">
+						{tr}Default View{/tr}
+					</label>
+					<div class="col-sm-8">
+						<select id="fgal_default_view" name="fgal_default_view" class="form-control">
+							<option value="list"{if $gal_info.default_view eq 'list'} selected="selected"{/if}>
+								{tr}List{/tr}
+							</option>
+							<option value="browse"{if $gal_info.default_view eq 'browse'} selected="selected"{/if}>
+								{tr}Browse{/tr}
+							</option>
+							<option value="page"{if $gal_info.default_view eq 'page'} selected="selected"{/if}>
+								{tr}Page{/tr}
+							</option>
+							{if $prefs.fgal_elfinder_feature eq 'y'}
+								<option value="finder"{if $gal_info.default_view eq 'finder'} selected="selected"{/if}>
+									{tr}Finder View{/tr}
+								</option>
+							{/if}
+						</select>
+					</div>
+				</div>
+				<div class="form-group">
 					<label for="sortorder" class="col-sm-4 text-right">{tr}Default sort order{/tr}</label>
 					<div class="col-sm-8">
 						<select name="sortorder" id="sortorder" class="form-control">
@@ -233,14 +256,24 @@ if ($(this).val() != '') {
 								<option value="{$item|escape}" {if $sortorder == $item} selected="selected"{/if}>{$key}</option>
 							{/foreach}
 						</select>
-						<br>
-						<input type="radio" id="sortdirection2" name="sortdirection" value="asc" {if $sortdirection == 'asc'}checked="checked"{/if}>
-						<label for="sortdirection2">{tr}Ascending{/tr}</label>
-						<br>
-						<input type="radio" id="sortdirection1" name="sortdirection" value="desc" {if $sortdirection == 'desc'}checked="checked"{/if}>
-						<label for="sortdirection1">{tr}Descending{/tr}</label>
+						<span class="help-block">
+							<label class="radio-inline" for="fgal_sortdirection1">
+								<input type="radio" id="fgal_sortdirection1" name="fgal_sortdirection" value="desc" {if $sortdirection == 'desc'}checked="checked"{/if} />
+								{tr}Descending{/tr}
+							</label>
+							<label class="radio-inline" for="fgal_sortdirection2">
+								<input type="radio" id="fgal_sortdirection2" name="fgal_sortdirection" value="asc" {if $sortdirection == 'asc'}checked="checked"{/if} />
+								{tr}Ascending{/tr}
+							</label>
+						</span>
 					</div>
 				</div>
+				<hr>
+				<div class="">
+					<label for="" class="control-label">{tr}Select which items to display when listing galleries{/tr}</label>
+					{include file='fgal_listing_conf.tpl'}
+				</div>
+				<hr>
 				<div class="form-group">
 					<label for="max_desc" class="col-sm-4 text-right">{tr}Max description display size{/tr}</label>
 					<div class="col-sm-8">
@@ -252,10 +285,6 @@ if ($(this).val() != '') {
 					<div class="col-sm-8">
 						<input type="text" id="maxRows" name="maxRows" value="{$maxRows|escape}" class="form-control">
 					</div>
-				</div>
-				<div class="">
-					<label for="" class="control-label">{tr}Select which items to display when listing galleries{/tr}</label>
-					{include file='fgal_listing_conf.tpl'}
 				</div>
 			{/tab}
 		{/tabset}

fgal_listing_conf.php
@@ -80,7 +80,6 @@ $fgal_options = array(
 	'show_explorer' => array('name' => tra('Explorer')),
 	'show_path' => array('name' => tra('Path')),
 	'show_slideshow' => array('name' => tra('Slideshow')),
-	'default_view' => array('name' => tra('Default View')),
 	'icon_fileId' => array('name' => tra('Gallery Icon')),
 );
 

templates/admin/include_fgal.tpl
@@ -159,6 +159,29 @@
 			{/remarksbox}
 
 			<input type="hidden" name="filegallistprefs" />
+			<div class="adminoptionbox clearfix margin-bottom-md">
+				<label class="col-sm-4 control-label" for="fgal_default_view">
+					{tr}Default View{/tr}
+				</label>
+				<div class="col-sm-8">
+					<select id="fgal_default_view" name="fgal_default_view" class="form-control">
+						<option value="list"{if $prefs.fgal_default_view eq 'list'} selected="selected"{/if}>
+							{tr}List{/tr}
+						</option>
+						<option value="browse"{if $prefs.fgal_default_view eq 'browse'} selected="selected"{/if}>
+							{tr}Browse{/tr}
+						</option>
+						<option value="page"{if $prefs.fgal_default_view eq 'page'} selected="selected"{/if}>
+							{tr}Page{/tr}
+						</option>
+						{if $prefs.fgal_elfinder_feature eq 'y'}
+							<option value="finder"{if $prefs.fgal_default_view eq 'finder'} selected="selected"{/if}>
+								{tr}Finder View{/tr}
+							</option>
+						{/if}
+					</select>
+				</div>
+			</div>
 			<div class="adminoptionbox clearfix">
 				<label for="fgal_sortorder" class="col-sm-4 control-label">{tr}Default sort order{/tr}:</label>
 				<div class="col-sm-8">

templates/fgal_listing_conf.tpl
@@ -3,29 +3,6 @@
 {if is_array($fgal_options) and count($fgal_options) gt 0}
 	{foreach key=key item=item from=$fgal_options}
 		{if $key eq 'default_view'}
-			<div class="form-group">
-				<label class="col-sm-4 control-label" for="fgal_{$key}">
-					{$item.name}
-				</label>
-				<div class="col-sm-8">
-					<select id="fgal_{$key}" name="fgal_{$key}" class="form-control">
-						<option value="list"{if $item.value eq 'list'} selected="selected"{/if}>
-							{tr}List{/tr}
-						</option>
-						<option value="browse"{if $item.value eq 'browse'} selected="selected"{/if}>
-							{tr}Browse{/tr}
-						</option>
-						<option value="page"{if $item.value eq 'page'} selected="selected"{/if}>
-							{tr}Page{/tr}
-						</option>
-						{if $prefs.fgal_elfinder_feature eq 'y'}
-							<option value="finder"{if $item.value eq 'finder'} selected="selected"{/if}>
-								{tr}Finder View{/tr}
-							</option>
-						{/if}
-					</select>
-				</div>
-			</div>
 		{elseif $key eq 'icon_fileId'}
 			<div class="form-group">
 				<label class="col-sm-4 control-label" for="fgal_{$key}">

commit 3027aab004f33bf5d8f7faa6b437c8a4294978fd

[FIX] filegals: Fix sort direction regression from r57569 (thanks Jyhem)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@58116 b456876b-0849-0410-b77d-98878d47e9d5

templates/edit_file_gallery.tpl
@@ -258,11 +258,11 @@ if ($(this).val() != '') {
 						</select>
 						<span class="help-block">
 							<label class="radio-inline" for="fgal_sortdirection1">
-								<input type="radio" id="fgal_sortdirection1" name="fgal_sortdirection" value="desc" {if $sortdirection == 'desc'}checked="checked"{/if} />
+								<input type="radio" id="fgal_sortdirection1" name="sortdirection" value="desc" {if $sortdirection == 'desc'}checked="checked"{/if} />
 								{tr}Descending{/tr}
 							</label>
 							<label class="radio-inline" for="fgal_sortdirection2">
-								<input type="radio" id="fgal_sortdirection2" name="fgal_sortdirection" value="asc" {if $sortdirection == 'asc'}checked="checked"{/if} />
+								<input type="radio" id="fgal_sortdirection2" name="sortdirection" value="asc" {if $sortdirection == 'asc'}checked="checked"{/if} />
 								{tr}Ascending{/tr}
 							</label>
 						</span>