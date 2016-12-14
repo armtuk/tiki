armtuk/tiki

commit 6a2266cd0f2ae081d573f994cd3abaad1646ddc4

[bp/r59676][ENH] trackers: Add a new option to Location fields to get a text address (from other fields in the same tracker) and attempt to automatically find the map location from that, either when saving or indexing. Thanks marclaporte and Xavi
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@59774 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tracker/Field/Location.php
@@ -19,7 +19,7 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 			'G' => array(
 				'name' => tr('Location'),
 				'description' => tr('Enable a geographic location to be selected for the item and displayed on a map.'),
-				'help' => 'Location Tracker Field',				
+				'help' => 'Location Tracker Field',
 				'prefs' => array('trackerfield_location'),
 				'tags' => array('basic'),
 				'default' => 'y',
@@ -62,6 +62,27 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 						'default' => 400,
 						'legacy_index' => 4,
 					),
+					'sourceFieldsList' => array(
+						'name' => tr('Fields To Search'),
+						'description' => tr('Fields in this tracker to use as a source to search for a location for.'),
+						'separator' => '|',
+						'filter' => 'int',
+						'profile_reference' => 'tracker_field',
+						'sort_order' => 'tracker_id',
+					),
+					'sourceSearchEvent' => array(
+						'name' => tr('When To Search'),
+						'description' => tr('Event to attempt to search for a location.'),
+						'filter' => 'alpha',
+						'default' => '',
+						'options' => array(
+							'' => tr('Never'),
+							'save' => tr('Save (when Location is empty)'),
+							'savealways' => tr('Save (always)'),
+							'index' => tr('Indexing (when Location is empty)'),
+							'indexalways' => tr('Indexing (always)'),
+						),
+					),
 				),
 			),
 		);
@@ -74,20 +95,20 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 		} else {
 			$value = $this->getValue();
 		}
-		
+
 		$parts = explode(',', $value);
 		$parts = array_map('floatval', $parts);
 
 		if (count($parts) >= 2) {
 			// Always use . as the decimal point in the value, and not comma as used some places
 			$value = '';
-			$value .= str_replace(',', '.', $parts[0]).',';
-			$value .= str_replace(',', '.', $parts[1]).',';
+			$value .= str_replace(',', '.', $parts[0]) . ',';
+			$value .= str_replace(',', '.', $parts[1]) . ',';
 			$value .= str_replace(',', '.', $parts[2]);
 
 			return array(
 				'value' => $value,
-				'x' => $parts[0],	
+				'x' => $parts[0],
 				'y' => $parts[1],
 				'z' => isset($parts[2]) ? $parts[2] : 0,
 			);
@@ -118,13 +139,76 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 
 			if (isset($attributes['tiki.icon.src'])) {
 				TikiLib::lib('smarty')->loadPlugin('smarty_modifier_escape');
-				$context['icon_data'] = ' data-icon-src="'. smarty_modifier_escape($attributes['tiki.icon.src']) .'"';
+				$context['icon_data'] = ' data-icon-src="' . smarty_modifier_escape($attributes['tiki.icon.src']) . '"';
 			} else {
 				$context['icon_data'] = '';
 			}
 
 			return $this->renderTemplate('trackeroutput/location.tpl', $context);
 		}
+	}
+
+	function handleSave($value, $oldValue)
+	{
+		$sourceFieldsList = $this->getOption('sourceFieldsList');
+
+		if ($sourceFieldsList) {
+			$event = $this->getOption('sourceSearchEvent');
+
+			$emptyValue = !$value || strpos($value, '0,0,') !== false;
+
+			if ($event === 'save' && $emptyValue || $event === 'savealways') {
+				$value = $this->searchForLocation($sourceFieldsList);
+			}
+		}
+
+		return array(
+			'value' => $value,
+		);
+
+	}
+
+	private function searchForLocation($sourceFieldsList)
+	{
+		global $prefs;
+		$out = '';
+		$address = '';
+
+		$definition = Tracker_Definition::get($this->getConfiguration('trackerId'));
+		$item = Tracker_Item::fromId($this->getItemId());
+		if ($item) {
+			$item = $item->getData();
+		}
+
+		array_walk($sourceFieldsList, function(& $field) use ($definition, $item, & $address) {
+
+			$fieldArray = $definition->getField($field);
+
+			if (! $fieldArray) {
+				$message = tr('Location: Field %0 not found for field "%1"', $field, $this->getConfiguration('permName'));
+				Feedback::error($message);
+			} else {
+
+				$factory = $definition->getFieldFactory();
+				$handler = $factory->getHandler($fieldArray, $item);
+
+				if ($handler) {
+					$fieldData = $handler->getFieldData($_REQUEST);
+					$address .= $fieldData['value'] . "\n";
+				}
+			}
+		});
+
+		if (trim($address)) {
+			$geo = TikiLib::lib('geo')->geocode($address);
+			if ($geo) {
+				$out = $geo['lon'] . ',' . $geo['lat'] . ',' . $prefs['gmap_defaultz'];
+			} else {
+				Feedback::error(tr('Could not find a location for "%0"', $address));
+			}
+		}
+
+		return $out;
 	}
 
 	function importRemote($value)
@@ -140,6 +224,37 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 	function importRemoteField(array $info, array $syncInfo)
 	{
 		return $info;
+	}
+
+	function getDocumentPart(Search_Type_Factory_Interface $typeFactory)
+	{
+		$value = $this->getValue();
+		$sourceFieldsList = $this->getOption('sourceFieldsList');
+
+		if ($sourceFieldsList) {
+			$emptyValue = !$value || strpos($value, '0,0,') !== false;
+			$event = $this->getOption('sourceSearchEvent');
+
+			if ($event === 'index' && $emptyValue || $event === 'indexalways') {
+				$newValue = $this->searchForLocation($sourceFieldsList);
+
+				if ($value !== $newValue) {
+					$value = $newValue;
+					$trklib = TikiLib::lib('trk');
+					$trklib->modify_field($this->getItemId(), $this->getConfiguration('fieldId'), $value);
+
+					// need to set up the geo attributes if this field is use_as_item_location
+					if ($this->getOption('use_as_item_location')) {
+						TikiLib::lib('geo')->set_coordinates('trackeritem', $this->getItemId(), $value);
+					}
+				}
+			}
+		}
+
+		$baseKey = $this->getBaseKey();
+		return array(
+			$baseKey => $typeFactory->sortable($value),	// TODO add geo_point type for elastic
+		);
 	}
 }
 

commit 4ce490058b103129a50263f83e178fce4799f0af

[FIX] trackers: Replace Feedback with errorreport used in 15.x (regression from r59774 - me again, sorry)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@60636 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tracker/Field/Location.php
@@ -188,7 +188,7 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 
 			if (! $fieldArray) {
 				$message = tr('Location: Field %0 not found for field "%1"', $field, $this->getConfiguration('permName'));
-				Feedback::error($message);
+				TikiLib::lib('errorreport')->report($message);
 			} else {
 
 				$factory = $definition->getFieldFactory();
@@ -206,7 +206,7 @@ class Tracker_Field_Location extends Tracker_Field_Abstract implements Tracker_F
 			if ($geo) {
 				$out = $geo['lon'] . ',' . $geo['lat'] . ',' . $prefs['gmap_defaultz'];
 			} else {
-				Feedback::error(tr('Could not find a location for "%0"', $address));
+				TikiLib::lib('errorreport')->report(tr('Could not find a location for "%0"', $address));
 			}
 		}
 